859e6e1eb7337b10b762e28656afab3dee2f07b7
hooosberg <zikedece@proton.me>
2026-08-20 15:33:52 +0800

    Add WeChat group QR expiry flow

---
+++ src/data/contactQr.ts
@@ -0,0 +1,6 @@
+export const wechatQr = {
+  image: "/images/contact/wechat-group-2026-08-20.jpg",
+  uploadedAt: "2026-08-20T00:00:00+08:00",
+  expiresAt: "2026-08-27T00:00:00+08:00",
+  validityDays: 7,
+};
